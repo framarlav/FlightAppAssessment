39009eaabdbfeff89cf738ee9bffc8cd4a30634b
Marquez Francisco <francisco.marquez@solera.com>
2022-10-31 16:47:44 +0100

    Booking API schedule

---
+++ backend/BookingMicroservice/booking/src/test/java/com/assesment/booking/BookingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.assesment.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
