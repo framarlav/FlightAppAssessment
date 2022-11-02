b88af1f051782f1295484747548d01da07540cd2
Marquez Francisco <francisco.marquez@solera.com>
2022-11-02 12:48:48 +0100

    Some AnalyticsAPI tests

---
+++ backend/AnalyticsMicroservice/analyticalInfo/src/main/java/com/example/analyticalInfo/AnalyticalInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.analyticalInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticalInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticalInfoApplication.class, args);
+	}
+
+}
